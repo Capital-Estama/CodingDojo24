bdf477a080dcd3cc23c6d9bb6d4e587310a2983f
Wes Est <wesest@Wess-MacBook-Air.local>
2024-02-18 19:34:15 -0500

    Promt six complete

--- Fundamentals_I/Program.cs
+++ Fundamentals_I/Program.cs
@@ -42,18 +42,90 @@ stopwatch.Restart();
 
 // Console style setup
 Console.Title = "Fundamentals I";
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.White;
 Console.BackgroundColor = ConsoleColor.DarkCyan;
+
 // Prompt 2: Create a new loop that generates 5 random numbers between 10 and 20.
+Console.WriteLine(" Create a new loop that generates 5 random numbers between 10 and 20.\n");
+Random rand = new Random();
+Func<int> nextNumber = () => rand.Next(10,21);
+for (int k = 1;k <=5; k++) {
+    Console.WriteLine(nextNumber());
+}
+stopwatch.Stop();
+ConsoleExcutionTime(stopwatch);
+stopwatch.Restart();
 
 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
 // Prompt 3: Modify the previous loop to add the random values together and print the sum after the loop finishes.
 
+Console.WriteLine("Modify the previous loop to add the random values together and print the sum after the loop finishes.\n");
+int randSum = 0;
+for (int k = 1;k <=5; k++) {
+    int i = nextNumber();
+    Console.WriteLine(i);
+    randSum += i;
+}
+Console.WriteLine($"sum after the loop: {randSum}\n");
+stopwatch.Stop();
+ConsoleExcutionTime(stopwatch);
+stopwatch.Restart();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
 // Prompt 4: Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but NOT both.
+Console.WriteLine(" Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but NOT both.\n\n");
+for(int l = 1; l <= 100; l++) {
+    if((l % 3 == 0 || l % 5 == 0 ) && !(l % 3 == 0 & l % 5 == 0)) {
+        Console.Write(l + ",");
+    }
+}
+stopwatch.Stop();
+ConsoleExcutionTime(stopwatch);
+stopwatch.Restart();
 
 // Prompt 5: Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for multiples of 5.
+Console.WriteLine(" Modify the previous loop to print \"Fizz\" for multiples of 3 and \"Buzz\" for multiples of 5.\n\n");
+for (int l = 1; l <= 100; l++) {
+    if((l % 3 == 0 || l % 5 == 0 ) && !(l % 3 == 0 & l % 5 == 0)) {
+        if(l % 3 == 0) {
+            Console.Write("Fizz\n");
+        }
+        else {
+            Console.Write("Buzz\n");
+        }
+        
+    }
+}
+stopwatch.Stop();
+ConsoleExcutionTime(stopwatch);
+stopwatch.Restart();
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 
 // Prompt 6: Modify the previous loop once more to now also print "FizzBuzz" for numbers that are multiples of both 3 and 5.
+Console.WriteLine(" Modify the previous loop to print \"Fizz\" for multiples of 3 and \"Buzz\" for multiples of 5.\n\n");
+for (int l = 1; l <= 100; l++) {
+    if (l % 3 == 0 & l % 5 == 0) {
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+    }
+        else if (l % 3 == 0) {
+            Console.Write("Fizz\n");
+        }
+        else if (l % 5 == 0) {
+            Console.Write("Buzz\n");
+        }
+        
+}
+
+stopwatch.Stop();
+ConsoleExcutionTime(stopwatch);
+stopwatch.Restart();
 
 // Prompt 7 (Optional): If you used for loops for your solutions, try doing the same with while loops. Vice versa if you used while loops!
 
--- Fundamentals_I/obj/Debug/net6.0/Fundamentals_I.AssemblyInfo.cs
+++ Fundamentals_I/obj/Debug/net6.0/Fundamentals_I.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentals_I")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af9496aa2794992f724bc12fdcca475f3cc45a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6ce20f27469556c335bfaf5acc2cefbe6aabe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentals_I")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentals_I")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
