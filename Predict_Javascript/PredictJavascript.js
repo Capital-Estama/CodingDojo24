5a6336f6eab09c6adcfecbf4ea8dec014e81a92e
Wes Est <wesest@Wess-MacBook-Air.local>
2024-02-19 18:06:39 -0500

    Predict Javascript

---
+++ Predict_Javascript/PredictJavascript.js
@@ -0,0 +1,4 @@
+//Problem 1
+
+console.log("Why did the code produce that output?\n If applicable, how would you get that index value that did not output?")
+
---
+++ Predict_Javascript/Predict_Javascript.js
@@ -0,0 +1,114 @@
+
+// Problem 1
+// Why did the code produce that output? If applicable, how would you get the index value that did not output?
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// Problem 2
+// Why did the code produce that output? If applicable, what would you need to do to solve any potential problems?
+
+const employee = {
+    employeeName: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { employeeName: otherName } = employee;
+//Predict the output
+console.log(otherName);
+console.log(employeeName);
+
+// Problem 3
+// Why did the code produce that output? If applicable, how would you alter this code without changing either console.log?
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);copy
+// Problem 4
+// Why did the code produce that output? Declare a new variable for the value at the 4th index of the array and console.log it.
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first === second);
+console.log(first === third);copy
+// Problem 5
+// Why did the code produce that output? Console.log the last value in the secondKey property's array.
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);copy
+// Problem 6
+// First, how many scopes does the following code block contain? Define each scope and guess what the output will be.
+
+var beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+  function actuallyPrintingNames() {
+    for (var index = 0; index < names.length; index++) {
+      var name = names[index];
+      console.log(name + ' was found at index ' + index);
+    }
+    console.log('name and index after loop is ' + name + ':' + index);
+  }
+  actuallyPrintingNames();
+}
+printNames(beatles);copy
+// Problem 7
+// Why did the code produce that output?
+
+ function actuallyPrintingNames() {
+  for (let index = 0; index < names.length; index++) {
+    let name = names[index];
+    console.log(name + ' was found at index ' + index);
+  }
+  console.log('name and index after loop is ' + name + ':' + index);
+}     copy
+// Problem 8
+// Why did the code produce that output? Explain the output, including any possible errors and why they occurred. If there are no errors, explain the justification for each keyword used to declare variables.
+
+const beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+  function actuallyPrintingNames() {
+    for (let index = 0; index < names.length; index++) {
+      const name = names[index];
+      console.log(name + ' was found at index ' + index);
+    }
+  }
+  actuallyPrintingNames();
+}
+printNames(beatles);copy
+// Problem 9
+// Why did the code produce that output? Explain why each console.log looks the way it does.
+
+const planet = {
+	name:"Jupiter",
+	milesFromSun: 49849,
+	mass: 393983,
+            composition: ["gas", "liquid", "oxygen"]
+}
+const planetCopy = {...planet}
+console.log(planet.composition[0] === planetCopy.composition[0]) 
+console.log(planet === planetCopy)
+
